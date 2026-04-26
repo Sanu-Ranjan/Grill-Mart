b0f209fc81ab1dce025de4c473662a509a9e36b4
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-26 11:16:58 +0530

    feat : add cart context to manage cart related features

--- client/ecom-project/src/contexts/CartContext.jsx
+++ client/ecom-project/src/contexts/CartContext.jsx
@@ -3,16 +3,25 @@ import { useFetch } from "../hooks/useFetch";
 import { API_BASE_URL } from "../constants/index";
 import { putData } from "../utils/putData";
 import { postData } from "../utils/postData";
+import { useToastAlert } from "./ToastAlertContext";
 const CartContext = createContext();
 const getCartUrl = `${API_BASE_URL}/cart`;
 
 export const CartProvider = ({ children }) => {
   const [refresh, setRefresh] = useState(false);
   const { data, loading, error } = useFetch(getCartUrl, refresh);
-
   const cart = data?.data?.cart?.[0]; // first cart doc
   const cartId = cart?._id; // needed for update/delete calls later
   const items = cart?.items ?? [];
+
+  const { addAlert } = useToastAlert();
+
+  const itemMap = new Map();
+  if (items.length > 0) {
+    items.forEach(({ product, quantity }) => {
+      itemMap.set(product._id, quantity);
+    });
+  }
 
   const addToCart = async (id) => {
     let found = false;
@@ -41,8 +50,10 @@ export const CartProvider = ({ children }) => {
       const { data, error } = await putData(updateCartUrl, body);
       if (error) return console.log("error updating cart : ", error);
 
-      if (data.success == true) setRefresh((prev) => !prev);
-      else console.log("Error updating cart : ", data.message);
+      if (data.success == true) {
+        setRefresh((prev) => !prev);
+        addAlert("Item added", "Cart");
+      } else console.log("Error updating cart : ", data.message);
     } catch (error) {
       console.log("Error: ", error);
     }
@@ -68,8 +79,10 @@ export const CartProvider = ({ children }) => {
     try {
       const { data, error } = await putData(updateCartUrl, body);
       if (error) return console.log("error updating cart : ", error);
-      if (data.success == true) setRefresh((prev) => !prev);
-      else console.log("Error updating cart : ", data.message);
+      if (data.success == true) {
+        setRefresh((prev) => !prev);
+        addAlert("An item removed", "Cart");
+      } else console.log("Error updating cart : ", data.message);
     } catch (error) {
       console.log("Error: ", error);
     }
@@ -85,18 +98,36 @@ export const CartProvider = ({ children }) => {
     });
 
     const body = {
-      items: items.filter(({ product }) => id != product._id),
+      items: newItemList.filter(({ product }) => id != product),
     };
     const updateCartUrl = `${API_BASE_URL}/cart/${cartId}`;
     try {
       const { data, error } = await putData(updateCartUrl, body);
       if (error) return console.log("error updating cart : ", error);
-      if (data.success == true) setRefresh((prev) => !prev);
-      else console.log("Error updating cart : ", data.message);
+      if (data.success == true) {
+        setRefresh((prev) => !prev);
+        addAlert("Item removed", "Cart");
+      } else console.log("Error updating cart : ", data.message);
     } catch (error) {
       console.log("Error: ", error);
     }
   };
+
+  const emptyCart = async () => {
+    const putCartUrl = `${API_BASE_URL}/cart/${cartId}`;
+    const body = {
+      items: [],
+    };
+    try {
+      const { data, error } = await putData(putCartUrl, body);
+      if (error) return console.log("Error emptying Cart : ", error);
+      if (data.success === true) setRefresh((prev) => !prev);
+    } catch (error) {
+      console.log("Error: ", error);
+    }
+  };
+
+  const buyNow = () => {};
 
   return (
     <CartContext.Provider
@@ -106,9 +137,11 @@ export const CartProvider = ({ children }) => {
         items,
         loading,
         error,
+        itemMap,
         addToCart,
         decQty,
         removeItem,
+        emptyCart,
       }}
     >
       {children}
