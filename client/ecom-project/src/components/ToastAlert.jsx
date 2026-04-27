4704bc0ac5b912d649be6a54089119c2e28338e8
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-27 18:43:16 +0530

    fix: add className keyword insted of class

--- client/ecom-project/src/components/ToastAlert.jsx
+++ client/ecom-project/src/components/ToastAlert.jsx
@@ -3,23 +3,23 @@ import { useToastAlert } from "../contexts/ToastAlertContext";
 const Alert = ({ message, subject, clear }) => {
   return (
     <div
-      class="toast fade show"
+      className="toast fade show"
       role="alert"
       aria-live="assertive"
       aria-atomic="true"
     >
-      <div class="toast-header">
-        <strong class="me-auto">{subject}</strong>
-        <small class="text-body-secondary">just now</small>
+      <div className="toast-header">
+        <strong className="me-auto">{subject}</strong>
+        <small className="text-body-secondary">just now</small>
         <button
           type="button"
-          class="btn-close"
+          className="btn-close"
           data-bs-dismiss="toast"
           aria-label="Close"
           onClick={() => clear()}
         ></button>
       </div>
-      <div class="toast-body">{message}</div>
+      <div className="toast-body">{message}</div>
     </div>
   );
 };
@@ -27,12 +27,12 @@ const Alert = ({ message, subject, clear }) => {
 export const ToastAlert = () => {
   const { messageQ, clearAlert } = useToastAlert();
   return (
-    <div aria-live="polite" aria-atomic="true" class="position-relative">
+    <div aria-live="polite" aria-atomic="true" className="position-relative">
       {/* <!-- Position it: -->
       <!-- - `.toast-container` for spacing between toasts -->
       <!-- - `top-0` & `end-0` to position the toasts in the upper right corner,  -->
       <!-- - `.p-3` to prevent the toasts from sticking to the edge of the container  --> */}
-      <div class="toast-container top-0 end-0 p-3">
+      <div className="toast-container top-0 end-0 p-3">
         {/* <!-- Then put toasts within --> */}
         {messageQ.length > 0 &&
           messageQ.map(({ message, subject }) => (
