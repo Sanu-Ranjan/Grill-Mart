54f80f8a3f89375ecfff9e80496211786bed606c
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-28 17:53:50 +0530

    fix: prevent overwrite on simultaneous button press for add to cart/inc/decrease/remove buttons

--- client/ecom-project/src/components/QuantityControls.jsx
+++ client/ecom-project/src/components/QuantityControls.jsx
@@ -1,9 +1,47 @@
+import { useRef, useState } from "react";
 import { useCart } from "../contexts/CartContext";
 
 export const QuantityControls = ({ product, btnWarning = true }) => {
-  const { addToCart, itemMap, decQty, removeItem } = useCart();
+  const [isBusy, setIsBusy] = useState(false);
+  const trackState = useRef([]);
+  const { addToCart, itemMap, decQty, removeItem, loading } = useCart();
   const isInCart = itemMap.has(product?._id);
   const quantity = itemMap.get(product?._id);
+
+  if (trackState.current.length === 0 && isBusy && loading)
+    trackState.current.push(1);
+  if (trackState.current.length === 1 && isBusy && !loading) {
+    setIsBusy(false);
+    trackState.current = [];
+  }
+
+  const handleAddtoCart = async () => {
+    setIsBusy(true);
+    try {
+      await addToCart(product?._id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRemoveItem = async () => {
+    setIsBusy(true);
+    try {
+      await removeItem(product?._id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handledecItem = async () => {
+    setIsBusy(true);
+    try {
+      await decQty(product?._id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       {isInCart ? (
@@ -11,30 +49,42 @@ export const QuantityControls = ({ product, btnWarning = true }) => {
           <button
             className="btn btn-outline-secondary btn-sm px-2 py-0"
             onClick={() => quantity > 1 && decQty(product?._id)}
+            disabled={isBusy}
           >
-            −
+            -
           </button>
           <span className="fw-semibold">{quantity}</span>
           <button
             className="btn btn-outline-secondary btn-sm px-2 py-0"
-            onClick={() => addToCart(product?._id)}
+            onClick={handleAddtoCart}
+            disabled={isBusy}
           >
             +
           </button>
           <button
             className="btn btn-outline-secondary btn-sm"
-            onClick={() => removeItem(product?._id)}
+            onClick={handleRemoveItem}
+            disabled={isBusy}
           >
-            Remove from cart
+            {isBusy ? (
+              <span className="spinner-border spinner-border-sm me-2" />
+            ) : (
+              "Remove from cart"
+            )}
           </button>
         </div>
       ) : (
         <button
           className={`btn btn-${btnWarning ? "warning" : "outline-secondary"} btn-sm fw-semibold`}
           style={{ width: "100%", height: "100%" }}
-          onClick={() => addToCart(product?._id)}
+          disabled={isBusy}
+          onClick={handleAddtoCart}
         >
-          Add to Cart
+          {isBusy ? (
+            <span className="spinner-border spinner-border-sm me-2" />
+          ) : (
+            "Add to Cart"
+          )}
         </button>
       )}
     </>
