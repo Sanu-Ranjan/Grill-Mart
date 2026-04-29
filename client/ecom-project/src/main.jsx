b0b095a77d17bf25b95e0c2cfcdf5f6e47575adc
Sanu-Ranjan <ranjan.code33@gmail.com>
2026-04-29 23:20:00 +0530

    feat:  including toast container an app root

--- client/ecom-project/src/main.jsx
+++ client/ecom-project/src/main.jsx
@@ -2,5 +2,10 @@ import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
+import { ToastContainer } from "react-toastify";
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <ToastContainer />
+  </>,
+);
